3e4280e061204be81df25f5e3a32bcb15f81ef55
Wojciech Trzesniowski <wojtek.trze@gmail.com>
2017-05-31 00:17:43 +0200

    change action and stock rows to relative layout

--- Resources/Resource.Designer.cs
+++ Resources/Resource.Designer.cs
@@ -2179,118 +2179,121 @@ namespace TDNoPV
 			public const int avd_show_password = 2130837588;
 			
 			// aapt resource value: 0x7f020055
-			public const int design_bottom_navigation_item_background = 2130837589;
+			public const int custom_button = 2130837589;
 			
 			// aapt resource value: 0x7f020056
-			public const int design_fab_background = 2130837590;
+			public const int design_bottom_navigation_item_background = 2130837590;
 			
 			// aapt resource value: 0x7f020057
-			public const int design_ic_visibility = 2130837591;
+			public const int design_fab_background = 2130837591;
 			
 			// aapt resource value: 0x7f020058
-			public const int design_ic_visibility_off = 2130837592;
+			public const int design_ic_visibility = 2130837592;
 			
 			// aapt resource value: 0x7f020059
-			public const int design_password_eye = 2130837593;
+			public const int design_ic_visibility_off = 2130837593;
 			
 			// aapt resource value: 0x7f02005a
-			public const int design_snackbar_background = 2130837594;
+			public const int design_password_eye = 2130837594;
 			
 			// aapt resource value: 0x7f02005b
-			public const int edit_icon = 2130837595;
+			public const int design_snackbar_background = 2130837595;
 			
 			// aapt resource value: 0x7f02005c
-			public const int edit_iconSmall = 2130837596;
+			public const int edit_icon = 2130837596;
 			
 			// aapt resource value: 0x7f02005d
-			public const int fab_shadow = 2130837597;
+			public const int edit_iconSmall = 2130837597;
 			
 			// aapt resource value: 0x7f02005e
-			public const int fab_shadow_mini = 2130837598;
+			public const int fab_shadow = 2130837598;
 			
 			// aapt resource value: 0x7f02005f
-			public const int finish_icon = 2130837599;
+			public const int fab_shadow_mini = 2130837599;
 			
 			// aapt resource value: 0x7f020060
-			public const int finish_iconSmall = 2130837600;
+			public const int finish_icon = 2130837600;
 			
 			// aapt resource value: 0x7f020061
-			public const int Icon = 2130837601;
+			public const int finish_iconSmall = 2130837601;
 			
 			// aapt resource value: 0x7f020062
-			public const int minus_icon = 2130837602;
+			public const int Icon = 2130837602;
 			
 			// aapt resource value: 0x7f020063
-			public const int minus_iconSmall = 2130837603;
+			public const int minus_icon = 2130837603;
 			
 			// aapt resource value: 0x7f020064
-			public const int navigation_empty_icon = 2130837604;
+			public const int minus_iconSmall = 2130837604;
 			
 			// aapt resource value: 0x7f020065
-			public const int no_icon = 2130837605;
+			public const int navigation_empty_icon = 2130837605;
 			
 			// aapt resource value: 0x7f020066
-			public const int no_iconSmall = 2130837606;
+			public const int no_icon = 2130837606;
 			
 			// aapt resource value: 0x7f020067
-			public const int notification_action_background = 2130837607;
+			public const int no_iconSmall = 2130837607;
 			
 			// aapt resource value: 0x7f020068
-			public const int notification_bg = 2130837608;
+			public const int notification_action_background = 2130837608;
 			
 			// aapt resource value: 0x7f020069
-			public const int notification_bg_low = 2130837609;
+			public const int notification_bg = 2130837609;
 			
 			// aapt resource value: 0x7f02006a
-			public const int notification_bg_low_normal = 2130837610;
+			public const int notification_bg_low = 2130837610;
 			
 			// aapt resource value: 0x7f02006b
-			public const int notification_bg_low_pressed = 2130837611;
+			public const int notification_bg_low_normal = 2130837611;
 			
 			// aapt resource value: 0x7f02006c
-			public const int notification_bg_normal = 2130837612;
+			public const int notification_bg_low_pressed = 2130837612;
 			
 			// aapt resource value: 0x7f02006d
-			public const int notification_bg_normal_pressed = 2130837613;
+			public const int notification_bg_normal = 2130837613;
 			
 			// aapt resource value: 0x7f02006e
-			public const int notification_icon_background = 2130837614;
-			
-			// aapt resource value: 0x7f020079
-			public const int notification_template_icon_bg = 2130837625;
-			
-			// aapt resource value: 0x7f02007a
-			public const int notification_template_icon_low_bg = 2130837626;
+			public const int notification_bg_normal_pressed = 2130837614;
 			
 			// aapt resource value: 0x7f02006f
-			public const int notification_tile_bg = 2130837615;
+			public const int notification_icon_background = 2130837615;
+			
+			// aapt resource value: 0x7f02007a
+			public const int notification_template_icon_bg = 2130837626;
+			
+			// aapt resource value: 0x7f02007b
+			public const int notification_template_icon_low_bg = 2130837627;
 			
 			// aapt resource value: 0x7f020070
-			public const int notify_panel_notification_icon_bg = 2130837616;
+			public const int notification_tile_bg = 2130837616;
 			
 			// aapt resource value: 0x7f020071
-			public const int plus_icon = 2130837617;
+			public const int notify_panel_notification_icon_bg = 2130837617;
 			
 			// aapt resource value: 0x7f020072
-			public const int plus_iconSmall = 2130837618;
+			public const int plus_icon = 2130837618;
 			
 			// aapt resource value: 0x7f020073
-			public const int start_icon = 2130837619;
+			public const int plus_iconSmall = 2130837619;
 			
 			// aapt resource value: 0x7f020074
-			public const int start_iconSmall = 2130837620;
+			public const int start_icon = 2130837620;
 			
 			// aapt resource value: 0x7f020075
-			public const int stop_icon = 2130837621;
+			public const int start_iconSmall = 2130837621;
 			
 			// aapt resource value: 0x7f020076
-			public const int stop_iconSmall = 2130837622;
+			public const int stop_icon = 2130837622;
 			
 			// aapt resource value: 0x7f020077
-			public const int yes_icon = 2130837623;
+			public const int stop_iconSmall = 2130837623;
 			
 			// aapt resource value: 0x7f020078
-			public const int yes_iconSmall = 2130837624;
+			public const int yes_icon = 2130837624;
+			
+			// aapt resource value: 0x7f020079
+			public const int yes_iconSmall = 2130837625;
 			
 			static Drawable()
 			{
@@ -2308,20 +2311,20 @@ namespace TDNoPV
 			// aapt resource value: 0x7f070073
 			public const int ActFragLV = 2131165299;
 			
-			// aapt resource value: 0x7f070075
-			public const int ActRowNameTV = 2131165301;
+			// aapt resource value: 0x7f070076
+			public const int ActRowNameTV = 2131165302;
 			
-			// aapt resource value: 0x7f070078
-			public const int ActRowRemoveBtn = 2131165304;
+			// aapt resource value: 0x7f070075
+			public const int ActRowRemoveBtn = 2131165301;
 			
 			// aapt resource value: 0x7f070074
 			public const int ActRowRunBtn = 2131165300;
 			
-			// aapt resource value: 0x7f070077
-			public const int ActRowStopwatchTV = 2131165303;
+			// aapt resource value: 0x7f070078
+			public const int ActRowStopwatchTV = 2131165304;
 			
-			// aapt resource value: 0x7f070076
-			public const int ActRowValueTV = 2131165302;
+			// aapt resource value: 0x7f070077
+			public const int ActRowValueTV = 2131165303;
 			
 			// aapt resource value: 0x7f07007c
 			public const int AddTaskDoneButton = 2131165308;
@@ -2419,17 +2422,17 @@ namespace TDNoPV
 			// aapt resource value: 0x7f0700c0
 			public const int StkRowDeleteBtn = 2131165376;
 			
-			// aapt resource value: 0x7f0700bf
-			public const int StkRowEditBtn = 2131165375;
-			
 			// aapt resource value: 0x7f0700bc
-			public const int StkRowNameTV = 2131165372;
-			
-			// aapt resource value: 0x7f0700be
-			public const int StkRowTimeTV = 2131165374;
+			public const int StkRowEditBtn = 2131165372;
 			
 			// aapt resource value: 0x7f0700bd
-			public const int StkRowValueTV = 2131165373;
+			public const int StkRowNameTV = 2131165373;
+			
+			// aapt resource value: 0x7f0700bf
+			public const int StkRowTimeTV = 2131165375;
+			
+			// aapt resource value: 0x7f0700be
+			public const int StkRowValueTV = 2131165374;
 			
 			// aapt resource value: 0x7f07008c
 			public const int action0 = 2131165324;
